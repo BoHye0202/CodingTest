93b5960327cb5258d84e748d170a5d4671efbf61
mimi_BH <0202cjstk@naver.com>
2021-04-11 17:58:55 +0900

    Level 1

---
+++ Programmers/[level1]문자열을 정수로 바꾸기.py
@@ -0,0 +1,6 @@
+def solution(s):
+    answer = int(s)
+    return print(answer)
+
+solution('1234')
+solution('-1234')
---
+++ Programmers/[level1]수박수박수.py
@@ -0,0 +1,7 @@
+def solution(n):
+    x = n//2 +1
+    answer = '수박'*x
+    return print(answer[:n])
+
+solution(3)
+solution(4)
