0c986f556ad3ef576f2db4d128d9f014e73a569f
mimi_BH <0202cjstk@naver.com>
2021-05-11 21:07:24 +0900

    Level 1

--- Programmers/[level1]완주하지 못한 선수.py
+++ Programmers/[level1]완주하지 못한 선수.py
@@ -2,8 +2,14 @@ from collections import Counter
 def solution(participant, completion):
     participant = Counter(participant)
     completion = Counter(completion)
-    answer = participant - completion
-    return answer.most_common()[0][0]
+    answer = participant-completion
+    return list(answer.keys())[0]
+
+# def solution(participant, completion):
+#     participant = Counter(participant)
+#     completion = Counter(completion)
+#     answer = participant - completion
+#     return answer.most_common()[0][0]
 
 solution(["leo", "kiki", "eden"], ["eden", "kiki"])
 solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"])
