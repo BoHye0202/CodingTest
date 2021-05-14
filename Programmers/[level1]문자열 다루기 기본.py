a0284c10f6c42d6c9e74c08d1ecec27151978992
mimi_BH <0202cjstk@naver.com>
2021-05-14 19:31:41 +0900

    Level 1

--- Programmers/[level1]문자열 다루기 기본.py
+++ Programmers/[level1]문자열 다루기 기본.py
@@ -1,5 +1,11 @@
 import re
 def solution(s):
+    check = re.search('[^0-9]', s)
+    if (len(s)==4 or len(s)==6) and check == None:
+        return True
+    return False
+
+def solution2(s):
     a = len(list(s))
     if a==4 or a==6:
         b = re.sub('[^0-9]','',s)
